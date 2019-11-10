a3e6534722d13cdbdb5b4258d7d1de36ae874d11
100milliongold <gadian88@gmail.com>
2019-11-10 19:31:45 +0900

    빙고게임 페이지 추가

---
+++ bingoGame/precache-manifest.a01baec8eaa7aa287d5f122f8ffe198f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6f0cff59d1d15297c19e39438c7fee",
+    "url": "/bingoGame/index.html"
+  },
+  {
+    "revision": "26d45ea876f7593b91d1",
+    "url": "/bingoGame/static/css/2.0f3d6610.chunk.css"
+  },
+  {
+    "revision": "ea9d3636fe4281ee2e7f",
+    "url": "/bingoGame/static/css/main.d14964b3.chunk.css"
+  },
+  {
+    "revision": "26d45ea876f7593b91d1",
+    "url": "/bingoGame/static/js/2.9ddd60b9.chunk.js"
+  },
+  {
+    "revision": "ea9d3636fe4281ee2e7f",
+    "url": "/bingoGame/static/js/main.5d390c57.chunk.js"
+  },
+  {
+    "revision": "d4867be9c2d97eaeea59",
+    "url": "/bingoGame/static/js/runtime-main.1e2c2cd1.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/bingoGame/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/bingoGame/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/bingoGame/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/bingoGame/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/bingoGame/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/bingoGame/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/bingoGame/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/bingoGame/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/bingoGame/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/bingoGame/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/bingoGame/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/bingoGame/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/bingoGame/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/bingoGame/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/bingoGame/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/bingoGame/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
