cf7ae77108b5330281e271cd489b5a2ac3ad3009
xiilab <je.kim@xiilab.com>
2020-04-10 08:31:12 +0900

    coinlist 업로드

---
+++ coinlist/precache-manifest.4efde33057b4c074c51124acaade14bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c6df4cec0b8595b443e91d59dbc61c",
+    "url": "/coinlist/index.html"
+  },
+  {
+    "revision": "6c8011cf4211e5f51efd",
+    "url": "/coinlist/static/css/main.551ea8ea.chunk.css"
+  },
+  {
+    "revision": "35d851128fd090d770af",
+    "url": "/coinlist/static/js/2.c3bc0589.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coinlist/static/js/2.c3bc0589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c8011cf4211e5f51efd",
+    "url": "/coinlist/static/js/main.c94c80b8.chunk.js"
+  },
+  {
+    "revision": "b7317f3e6a476254638b",
+    "url": "/coinlist/static/js/runtime-main.5b0685e5.js"
+  }
+]);
